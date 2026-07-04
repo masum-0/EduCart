b30fb0fb1bf813396b2d214266a6702483b3d6d7
Masum <almasumpqr@gmail.com>
2026-07-05 05:11:53 +0600

    app_theme.dart added

---
+++ lib/screens/app_theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const Color primaryBlue = Color(0xFF2100B8);
+const Color accentBlue = Color(0xFF6E8CFF);
+const Color lightGrey = Color(0xFFF2F2F2);
+const Color pinkColor = Color(0xFFFF7D8B);
+
+const List<String> productCategories = [
+  'Textbook',
+  'Notebook',
+  'Question Bank',
+  'Digital Resource',
+  'Other',
+];
+
+const List<String> productConditions = [
+  'New',
+  'Used - Good',
+  'Used - Fair',
+];
